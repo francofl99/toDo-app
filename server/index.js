d2584e7eea649bee2ef7262505c479c4d22a18c5
Franco Fernandez Lorio <franco@pop-os.localdomain>
2020-12-27 11:15:39 -0300

    backend ready

--- server/index.js
+++ server/index.js
@@ -43,3 +43,42 @@ app.post('/todo/add', (req, res) => {
         }
     })
 })
+
+app.get('/todo/completed', (req, res) => {
+    todoModel.find({ completed: true }, (err, todos) => {
+        if (err) {
+            res.send("Error while fetching Todos")
+        } else {
+            res.json(todos)
+        }
+    })
+})
+
+app.get('/todo/uncomplete', (req, res) => {
+    todoModel.find({ completed: false }, (err, todos) => {
+        if (err) {
+            res.send("Error while fetching Todos")
+        } else {
+            res.json(todos)
+        }
+    })
+})
+
+app.post('/todo/complete/:id', (req, res) => {
+    todoModel.findByIdAndUpdate(req.params.id, {completed : true}, (err, todo) => {
+        if (!err) {
+            res.send("Good work")
+        }
+    })
+})
+
+app.delete('/todo/:id', (req, res) => {
+    let query = {_id: req.params.id}
+    todoModel.deleteOne(query, (err) => {
+        if (err) {
+            res.send("Error while deleting todo")
+        } else {
+            res.send("Todo deleted")
+        }
+    })
+})
