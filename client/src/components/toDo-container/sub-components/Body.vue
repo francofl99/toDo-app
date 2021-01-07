77f7aea138fe8e1b78bee9b70d01e4cb84b750ea
Franco <franco@localhost.localdomain>
2021-01-07 20:46:48 -0300

    single draggable working

--- client/src/components/toDo-container/sub-components/Body.vue
+++ client/src/components/toDo-container/sub-components/Body.vue
@@ -1,32 +1,41 @@
 <template>
-  <transition-group
+  <draggable
+    :animation="300"
+    v-model="toDoList"
     class="container-body"
-    enter-active-class="animate__animated animate__fadeInDown animate__faster"
-    leave-active-class="animate__animated animate__fadeOutRight animate__faster"
+    ghost-class="moving-card"
   >
-    <div
-      v-for="toDo in this.toDosCompleted
-        ? this.completedToDos
-        : this.uncompletedToDos"
-      :key="toDo._id"
+    <transition-group
+      enter-active-class="animate__animated animate__fadeInDown animate__faster"
+      leave-active-class="animate__animated animate__fadeOutRight animate__faster"
     >
-      <ToDoCard :toDo="toDo" />
-    </div>
-  </transition-group>
+      <div v-for="toDo in toDoList" :key="toDo._id">
+        <ToDoCard :toDo="toDo" />
+      </div>
+    </transition-group>
+  </draggable>
 </template>
 
 <script>
+import draggable from "vuedraggable";
+
 import ToDoCard from "../../toDo-card/ToDoCard";
 
 export default {
   name: "Body",
 
   computed: {
-    uncompletedToDos() {
-      return this.$store.getters.getUncompletedToDos;
-    },
-    completedToDos() {
-      return this.$store.getters.getCompleteToDos;
+    toDoList: {
+      get() {
+        return this.toDosCompleted
+          ? this.$store.getters.getCompleteToDos
+          : this.$store.getters.getUncompletedToDos;
+      },
+      set(value) {
+        return this.toDosCompleted
+          ? this.$store.commit("setCompletedToDos", value)
+          : this.$store.commit("setUncompletedToDos", value);
+      },
     },
   },
 
@@ -34,6 +43,7 @@ export default {
 
   components: {
     ToDoCard,
+    draggable,
   },
 
   methods: {
@@ -54,5 +64,9 @@ export default {
 <style scoped lang="postcss">
 .container-body {
   @apply flex flex-col row-span-4 overflow-y-auto px-4 scrollbar;
+}
+
+.moving-card {
+  @apply rounded-md opacity-50 bg-gray-200;
 }
 </style>
