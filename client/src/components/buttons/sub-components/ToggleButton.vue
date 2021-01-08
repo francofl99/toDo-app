aa4f471ffd23030a05a76f8907fa87607f30b6aa
Franco <franco@localhost.localdomain>
2021-01-08 15:42:49 -0300

    toggle button finished

--- client/src/components/buttons/sub-components/ToggleButton.vue
+++ client/src/components/buttons/sub-components/ToggleButton.vue
@@ -1,20 +1,52 @@
 <template>
-  <!-- Container -->
-  <div class="flex justify-between items-center px-8 py-6">
-    <!-- Title -->
-    <div class=" text-xl font-bold text-gray-200 mr-2">Dark Mode</div>
+  <div class="container">
+    <div class="header">Dark Mode</div>
 
-    <!-- Switch Container -->
-    <div class="w-16 h-8 bg-gray-300 rounded-full flex-shrink-0 p-1">
-      <!-- Switch -->
-      <div class="bg-white w-6 h-6 rounded-full shadow-md"></div>
+    <div
+      :class="{ toggleContainerOnActiveMode: toggleActive }"
+      @click="toggleActive = !toggleActive"
+      class="toggle-container"
+    >
+      <div
+        :class="{ toggleButtonOnActiveMode: toggleActive }"
+        class="toggle-button"
+      ></div>
     </div>
-    <!-- Switch Container End -->
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  name: "ToggleButton",
+
+  data: () => ({
+    toggleActive: false,
+  }),
+};
 </script>
 
-<style></style>
+<style scoped lang="postcss">
+.container {
+  @apply flex items-center justify-end py-6 w-2/6;
+}
+
+.header {
+  @apply text-xl font-bold text-gray-200 mr-2;
+}
+
+.toggle-container {
+  @apply w-16 h-8 duration-300 ease-in-out bg-gray-300 rounded-full flex-shrink-0 p-1;
+}
+
+.toggle-button {
+  @apply duration-300 ease-in-out  transform bg-white w-6 h-6 rounded-full shadow-md;
+}
+
+.toggleContainerOnActiveMode {
+  @apply bg-yellow-400;
+}
+
+.toggleButtonOnActiveMode {
+  @apply translate-x-8;
+}
+</style>
